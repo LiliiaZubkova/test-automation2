75790b502da13d60198061acd7f4bacb56fb2119
LiliiaZubkova <life7773@gmail.com>
2018-03-27 19:34:46 +0300

    Test

---
+++ src/test/java/Lesson5/FirstTest.java
@@ -0,0 +1,48 @@
+package Lesson5;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
+public class FirstTest {
+
+    static  WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void firstResultShouldContainExpectedText() {
+        driver.findElement(By.id("search_query_top")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys("Dress");
+        driver.findElement(By.id("search_query_top")).submit();
+        assertThat(driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a")).getText(),
+                containsString("Dress"));
+
+    }
+}
+
+
